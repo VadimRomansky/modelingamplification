c14f53ee3f62e6d398fd0a248bb1edbf81bb35c5
romanskyvadim@gmail.com <romanskyvadim@gmail.com@cd62ec03-6e9b-389a-dd97-e0453ba2a678>
2012-09-23 16:40:10 +0000

    2d bugs fixed

--- Magnetic field amplification 2dflat/output.h
+++ Magnetic field amplification 2dflat/output.h
@@ -12,7 +12,7 @@ void outputEnergyPDF(std::vector <Particle*>& list,const char* fileName);
 void outputStartPDF(std::list <Particle*>& l,const char* fileName, Simulation& simulation,double minp,double maxp);
 void outputParticlePath(std::list<Particle*>& list,const char* cosmicRayFileName,const char* notCosmicRayFileName);
 void outputRadialProfile(SpaceBin** bins, FILE* outProfile, const int rgridNumber);
-//void outputProfile(SpaceBins*** bins, const int xgridNumber, const int ygridNumber);
+void outputProfile(SpaceBin*** bins, const int xgridNumber, const int ygridNumber);
 void outputShockWave(std::list<double> points, std::list<double> velocity);
 void outputAverageVz(double minp, double maxp, double* averageVz, const char* fileName);
 void outputParticles(std::vector<Particle*>& particles, const char* fileName);
