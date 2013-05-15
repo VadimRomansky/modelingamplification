a7a7fe8c06e9100b7f1eeffe4aa1d41bcce254d2
romanskyvadim@gmail.com <romanskyvadim@gmail.com@cd62ec03-6e9b-389a-dd97-e0453ba2a678>
2013-05-15 20:42:31 +0000

    some new

--- Zirakashvili/Simulation.cpp
+++ Zirakashvili/Simulation.cpp
@@ -15,6 +15,26 @@ void Simulation::simulate(){
 }
 
 void Simulation::solveUpstream1(){
+	double* density = new double[rgridNumber];
+	double* velocity = new double[rgridNumber];
+	double* pressure = new double[rgridNumber];
+	for(int i = 0; i < rgridNumber; ++i){
+		double H = upstreamBins1[i]->density*qube(oldReverseShockWaveR/reverseShockWaveR) - density[i-1]*(3*tau*upstreamBins1[i+1]->xi*upstreamBins1[i+1]->xi*(velocity[i+1] - forwardV*upstreamBins1[i+1]->xi)/(qube(upstreamBins1[i+1]->xi) - qube(upstreamBins1[i]->xi)));
+		velocity[i] = (upstreamBins1[i]->density*upstreamBins1[i]->U*exp(-3*tau) - density[i+1]*velocity[i+1]*3*tau*upstreamBins1[i+1]->xi*upstreamBins1[i+1]->xi/(forwardV*(qube(upstreamBins1[i+1]->xi) - qube(upstreamBins1[i]->xi)))-(pressure[i+1] - upstreamBins1[i+1]->pressure)*tau/(forwardV*(upstreamBins1[i+1]->xi - upstreamBins1[i]->xi)))/H;
+		density[i] = H/(1 - 3*tau*upstreamBins1[i+1]->xi*upstreamBins1[i+1]->xi*(velocity[i] - forwardV*upstreamBins1[i]->xi)/(forwardV*(qube(upstreamBins1[i+1]->xi) - qube(upstreamBins1[i]->xi))));
+		pressure[i] = (upstreamBins1[i]->pressure*exp(-3*tau) - pressure[i+1]*3*tau*sqr(upstreamBins1[i+1]->xi)*(velocity[i+1] - forwardV*upstreamBins1[i+1]->xi)/(forwardV*(qube(upstreamBins1[i+1]->xi) - qube(upstreamBins1[i]->xi))))*
+			(1 - 3*tau*(sqr(upstreamBins1[i]->xi)*(velocity[i] - forwardV*upstreamBins1[i]->xi) + (gamma - 1)*(sqr(upstreamBins1[i+1]->xi)*upstreamBins1[i+1]->U - sqr(upstreamBins1[i]->xi)*upstreamBins1[i]->U))/(forwardV*(qube(upstreamBins1[i+1]->xi) - qube(upstreamBins1[i]->xi))));
+	}
+
+	for(int i = 0; i < rgridNumber; ++i){
+		upstreamBins1[i]->U = velocity[i];
+		upstreamBins1[i]->density = density[i];
+		upstreamBins1[i]->pressure = pressure[i];
+	}
+
+	delete[] density;
+	delete[] velocity;
+	delete[] pressure;
 }
 
 void Simulation::solveUpstream2(){
@@ -25,7 +45,7 @@ void Simulation::solveUpstream2(){
 		double G = upstreamBins2[i]->density*exp(-3*tau) - density[i+1]*(3*tau*upstreamBins2[i+1]->xi*upstreamBins2[i+1]->xi*(velocity[i+1] - forwardV*upstreamBins2[i+1]->xi)/(qube(upstreamBins2[i+1]->xi) - qube(upstreamBins2[i]->xi)));
 		velocity[i] = (upstreamBins2[i]->density*upstreamBins2[i]->U*exp(-3*tau) - density[i+1]*velocity[i+1]*3*tau*upstreamBins2[i+1]->xi*upstreamBins2[i+1]->xi/(forwardV*(qube(upstreamBins2[i+1]->xi) - qube(upstreamBins2[i]->xi)))-(pressure[i+1] - upstreamBins2[i+1]->pressure)*tau/(forwardV*(upstreamBins2[i+1]->xi - upstreamBins2[i]->xi)))/G;
 		density[i] = G/(1 - 3*tau*upstreamBins2[i+1]->xi*upstreamBins2[i+1]->xi*(velocity[i] - forwardV*upstreamBins2[i]->xi)/(forwardV*(qube(upstreamBins2[i+1]->xi) - qube(upstreamBins2[i]->xi))));
-		pressure[i] = (upstreamBins2[i]->pressure*exp(-3*tau) - pressure[i+1]*3*tau*sqr(upstreamBins2[i+1]->xi)*(velocity[i+1] - forwardV*upstreamBins2[i+1]->xi)/(forwardV*(qube(upstreamBins2[i+1]->xi) - qube(upstreamBins2[i]->xi))))
+		pressure[i] = (upstreamBins2[i]->pressure*exp(-3*tau) - pressure[i+1]*3*tau*sqr(upstreamBins2[i+1]->xi)*(velocity[i+1] - forwardV*upstreamBins2[i+1]->xi)/(forwardV*(qube(upstreamBins2[i+1]->xi) - qube(upstreamBins2[i]->xi))))*
 			(1 - 3*tau*(sqr(upstreamBins2[i]->xi)*(velocity[i] - forwardV*upstreamBins2[i]->xi) + (gamma - 1)*(sqr(upstreamBins2[i+1]->xi)*upstreamBins2[i+1]->U - sqr(upstreamBins2[i]->xi)*upstreamBins2[i]->U))/(forwardV*(qube(upstreamBins2[i+1]->xi) - qube(upstreamBins2[i]->xi))));
 	}
 
