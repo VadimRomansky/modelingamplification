2e9f747da817d5e6b11b70c76169c6c616cbbc4a
romanskyvadim@gmail.com <romanskyvadim@gmail.com@cd62ec03-6e9b-389a-dd97-e0453ba2a678>
2013-06-22 14:30:15 +0000

    initial conditions

--- branches/v3/Zirakashvili/output.cpp
+++ branches/v3/Zirakashvili/output.cpp
@@ -4,15 +4,15 @@
 
 void output(FILE* outFile, Simulation* simulation){
 	for(int i = 0; i < simulation->rgridNumber; ++i){
-		fprintf(outFile,"%17.12lf %17.12lf %17.12lf %17.12lf %17.12lf\n", simulation->upstreamBins1[i]->r, simulation->upstreamBins1[i]->U, simulation->upstreamBins1[i]->density, simulation->upstreamBins1[i]->pressure, simulation->upstreamBins1[i]->temperature);
+		fprintf(outFile,"%17.12lf %17.12lf %38.30lf %17.12lf %17.12lf\n", simulation->upstreamBins1[i]->r, simulation->upstreamBins1[i]->U, simulation->upstreamBins1[i]->density, simulation->upstreamBins1[i]->pressure, simulation->upstreamBins1[i]->temperature);
 	}
 	for(int i = 0; i < simulation->rgridNumber; ++i){
-		fprintf(outFile,"%17.12lf %17.12lf %17.12lf %17.12lf %17.12lf\n", simulation->downstreamBins1[i]->r, simulation->downstreamBins1[i]->U, simulation->downstreamBins1[i]->density, simulation->downstreamBins1[i]->pressure, simulation->downstreamBins1[i]->temperature);
+		fprintf(outFile,"%17.12lf %17.12lf %38.30lf %17.12lf %17.12lf\n", simulation->downstreamBins1[i]->r, simulation->downstreamBins1[i]->U, simulation->downstreamBins1[i]->density, simulation->downstreamBins1[i]->pressure, simulation->downstreamBins1[i]->temperature);
 	}
 	for(int i = 0; i < simulation->rgridNumber; ++i){
-		fprintf(outFile,"%17.12lf %17.12lf %17.12lf %17.12lf %17.12lf\n", simulation->downstreamBins2[i]->r, simulation->downstreamBins2[i]->U, simulation->downstreamBins2[i]->density, simulation->downstreamBins2[i]->pressure, simulation->downstreamBins2[i]->temperature);
+		fprintf(outFile,"%17.12lf %17.12lf %38.30lf %17.12lf %17.12lf\n", simulation->downstreamBins2[i]->r, simulation->downstreamBins2[i]->U, simulation->downstreamBins2[i]->density, simulation->downstreamBins2[i]->pressure, simulation->downstreamBins2[i]->temperature);
 	}
 	for(int i = 0; i < simulation->rgridNumber; ++i){
-		fprintf(outFile,"%17.12lf %17.12lf %17.12lf %17.12lf %17.12lf\n", simulation->upstreamBins2[i]->r, simulation->upstreamBins2[i]->U, simulation->upstreamBins2[i]->density, simulation->upstreamBins2[i]->pressure, simulation->upstreamBins2[i]->temperature);
+		fprintf(outFile,"%17.12lf %17.12lf %38.30lf %17.12lf %17.12lf\n", simulation->upstreamBins2[i]->r, simulation->upstreamBins2[i]->U, simulation->upstreamBins2[i]->density, simulation->upstreamBins2[i]->pressure, simulation->upstreamBins2[i]->temperature);
 	}
 }
