78dfe958db94bd402808e13e02625ca2df7ce6e5
romanskyvadim@gmail.com <romanskyvadim@gmail.com@cd62ec03-6e9b-389a-dd97-e0453ba2a678>
2013-10-06 11:10:58 +0000

    distribution function and bug with closing file

--- Godunov1d/output.cpp
+++ Godunov1d/output.cpp
@@ -10,3 +10,13 @@ void output(FILE* outFile, Simulation* simulation){
 		//fprintf(outFile,"%lf %lf %lf %lf %lf\n", simulation->grid[i], simulation->middleVelocity[i], simulation->middleDensity[i], simulation->middlePressure[i], simulation->temperatureIn(i));
 	}
 }
+
+void outputDistribution(FILE* distributionFile, Simulation* simulation){
+	for(int i = 0; i < simulation->rgridNumber; ++i){
+		double p = simulation->minP;
+		double deltaP = (simulation->maxP - simulation->minP)/(pgridNumber - 1);
+		for(int j = 0; j < pgridNumber; ++j){
+			fprintf(distributionFile, "%17.12lf %17.12\n", p, simulation->distributionFunction[i][j]);
+		}
+	}
+}
