39ac69e3e9b40c92e890df25f216ed3ba1af4c74
romanskyvadim@gmail.com <romanskyvadim@gmail.com@cd62ec03-6e9b-389a-dd97-e0453ba2a678>
2014-01-30 18:17:30 +0000

    shock wave point

--- GodunovSpherical/constants.h
+++ GodunovSpherical/constants.h
@@ -24,6 +24,6 @@ const double defaultMinK = 0.1E-8;
 const double turbulenceSeed = 0.000000000001;
 const int generationSize = 2;
 const double momentumParameter = 5;
-const int writeParameter = 2000;
+const int writeParameter = 10000;
 
 #endif
