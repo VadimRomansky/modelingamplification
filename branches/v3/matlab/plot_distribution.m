520cc1e590f6b59d07fe0513ca6fe287b617fb68
romanskyvadim@gmail.com <romanskyvadim@gmail.com@cd62ec03-6e9b-389a-dd97-e0453ba2a678>
2014-02-10 23:39:39 +0000

    matlab

--- branches/v3/matlab/plot_distribution.m
+++ branches/v3/matlab/plot_distribution.m
@@ -5,19 +5,30 @@ load coordinateDistribution.dat;
 N1=1;
 N2=1000;
 N3=100;
-e = N2 - 1;
-d = fix(3*N2/4);
-c = fix(N2/2);
-b = fix(N2/4);
-a = 5;
+
 
 N4 = size(fullDistribution,1);
 a1 = 1;
 a2 = fix(N4/(2*N3));
 a3 = fix(N4/N3) - 1;
 
+fun(1:N3,1:2) = 0;
+
+for i = 1: N3,
+    fun(i,1) = fullDistribution(a3*N3 + i,1);
+    fun(i,2) = fullDistribution(a3*N3 + i,2)*(fun(i,1)^4);
+end
+
+figure(1);
+plot (fun(1:N3,1), fun(1:N3,2),'red');
+title ('f(p)*p^4');
+xlabel ('p g*cm/s');
+ylabel ('f');
+grid ;
+
+
 figure(2);
-plot (fullDistribution(1:N3,1),fullDistribution(1:N3,2),'red',fullDistribution(1:N3,1),fullDistribution(a1*N3 + (1:N3),2),'green',fullDistribution(1:N3,1),fullDistribution(a2*N3 + (1:N3),2),'black',fullDistribution(1:N3,1),fullDistribution(a3*N3 + (1:N3),2),'blue');
+plot (fullDistribution(1:N3,1),fullDistribution(1:N3,2),'red',fullDistribution(a1*N3 + (1:N3),1),fullDistribution(a1*N3 + (1:N3),2),'green',fullDistribution(a2*N3 + (1:N3),1),fullDistribution(a2*N3 + (1:N3),2),'black',fullDistribution(a3*N3 + (1:N3),1),fullDistribution(a3*N3 + (1:N3),2),'blue');
 title ('f(p)');
 xlabel ('p g*cm/s');
 ylabel ('f');
