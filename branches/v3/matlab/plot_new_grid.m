9dcc36b07b080d176f11c0362fdfc3c826462daf
romanskyvadim@gmail.com <romanskyvadim@gmail.com@cd62ec03-6e9b-389a-dd97-e0453ba2a678>
2014-02-19 20:40:43 +0000

    Hz

--- branches/v3/matlab/plot_new_grid.m
+++ branches/v3/matlab/plot_new_grid.m
@@ -1,13 +1,13 @@
 clear;
 load temp_grid.dat;
 N1=1;
-N2=1000;
+N2=100;
 N3=6;
 e = size(temp_grid,1)/N2 - 1;
 d = fix(3*e/4);
 c = fix(e/2);
 b = fix(e/4);
-a = 5;
+a = fix(e/8);
 
 figure(1);
 plot (temp_grid(1:N2,1),temp_grid(a*N2 + (1:N2),2),'cyan',temp_grid(1:N2,1),temp_grid(b*N2 + (1:N2),2),'green',temp_grid(1:N2,1),temp_grid(c*N2 + (1:N2),2),'blue',temp_grid(1:N2,1),temp_grid(d*N2 + (1:N2),2),'black',temp_grid(1:N2,1),temp_grid(e*N2 + (1:N2),2),'red');
@@ -17,7 +17,7 @@ ylabel ('r cm');
 grid ;
 
 figure(2);
-plot (temp_grid(a*N2 + (1:N2),2), temp_grid(1:N2,1),'cyan',temp_grid(b*N2 + (1:N2),2),temp_grid(1:N2,1),'green',temp_grid(c*N2 + (1:N2),2),temp_grid(1:N2,1),'blue',temp_grid(d*N2 + (1:N2),2),temp_grid(1:N2,1),'black',temp_grid(e*N2 + (1:N2),2),temp_grid(1:N2,1),'red');
+plot (temp_grid(1:N2,2), temp_grid(1:N2,1),'cyan',temp_grid(b*N2 + (1:N2),2),temp_grid(1:N2,1),'green',temp_grid(c*N2 + (1:N2),2),temp_grid(1:N2,1),'blue',temp_grid(d*N2 + (1:N2),2),temp_grid(1:N2,1),'black',temp_grid(e*N2 + (1:N2),2),temp_grid(1:N2,1),'red');
 title ('r');
 xlabel ('r cm');
 ylabel ('i');
