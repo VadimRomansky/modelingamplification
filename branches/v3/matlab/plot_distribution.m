7b99054a26d20942e94abc20925bf8e5cbc68fd6
romanskyvadim@gmail.com <romanskyvadim@gmail.com@cd62ec03-6e9b-389a-dd97-e0453ba2a678>
2014-03-10 09:25:46 +0000

    hz

--- branches/v3/matlab/plot_distribution.m
+++ branches/v3/matlab/plot_distribution.m
@@ -11,21 +11,23 @@ c = 3*10^10;
 
 
 N4 = size(fullDistribution,1);
-a1 = 1;
+a1 = fix(N4/(4*N3));
 a2 = fix(N4/(2*N3));
 a3 = fix(N4/N3) - 1;
 
-fun(1:N3,1:2) = 0;
+fun(1:N3,1:4) = 0;
 
 for i = 1: N3,
     fun(i,1) = fullDistribution(a3*N3 + i,1)/(m*c);
-    fun(i,2) = fullDistribution(a3*N3 + i,2)*(fun(i,1)^4);
+    fun(i,2) = fullDistribution(a1*N3 + i,2)*(fun(i,1)^4);
+    fun(i,3) = fullDistribution(a2*N3 + i,2)*(fun(i,1)^4);
+    fun(i,4) = fullDistribution(a3*N3 + i,2)*(fun(i,1)^4);
 end
 
 figure(1);
-plot (fun(1:N3,1), fun(1:N3,2),'red');
+plot (fun(1:N3,1), fun(1:N3,2),'blue', fun(1:N3,1), fun(1:N3,3),'green', fun(1:N3,1), fun(1:N3,4),'red');
 title ('f(p)*p^4');
-xlabel ('p g*cm/s');
+xlabel ('p/mc');
 ylabel ('f');
 grid ;
 
@@ -38,8 +40,8 @@ ylabel ('f');
 grid ;
 
 figure(3);
-plot (coordinateDistribution(1:N2,1),coordinateDistribution(1:N2,2),'red');
-title ('f(p)');
-xlabel ('r cm');
+plot (fun(1:N3,1), fun(1:N3,4),'red');
+title ('f(p)*p^4');
+xlabel ('p/mc');
 ylabel ('f');
 grid ;
