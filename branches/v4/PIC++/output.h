8255f530f43961d4e00520fb12ec57ed036c5da4
romanskyvadim@gmail.com <romanskyvadim@gmail.com@cd62ec03-6e9b-389a-dd97-e0453ba2a678>
2014-09-11 14:50:13 +0000

    compile

--- branches/v4/PIC++/output.h
+++ branches/v4/PIC++/output.h
@@ -4,9 +4,13 @@
 #include "vector"
 #include "stdio.h"
 
+#include "vector3d.h"
 #include "particle.h"
 
 void outputDistribution(FILE* outFile, std::vector<Particle*> particles, int particleType);
 void outputTraectory(FILE* outFile, Particle* particle, double time);
+void outputGrid(FILE* outFile, double* grid, int number);
+void outputFields(FILE* outEfile, FILE* outBfile, Vector3d*** Efield, Vector3d*** Bfield, int xnumber, int ynumber, int znumber);
+
 
 #endif
