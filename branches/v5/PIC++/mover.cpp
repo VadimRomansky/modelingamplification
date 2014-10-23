e0e6add2e30673132bf28983ce4b5c624965005c
romanskyvadim@gmail.com <romanskyvadim@gmail.com@cd62ec03-6e9b-389a-dd97-e0453ba2a678>
2014-10-23 15:59:46 +0000

    fz

--- branches/v5/PIC++/mover.cpp
+++ branches/v5/PIC++/mover.cpp
@@ -23,24 +23,24 @@ void Simulation::moveParticle(Particle* particle){
 	Vector3d B = correlationBfield(particle);
 
 	double gamma = particle->gammaFactor(speed_of_light_normalized);
-	Vector3d velocity = particle->velocity(speed_of_light_normalized);
+	Vector3d velocity = particle->momentum/(particle->mass*gamma);
 
 	particle->oldCoordinates = particle->coordinates;
 	particle->coordinates += velocity*deltaT;
 	Matrix3d matrix;
 
-	double beta = particle->charge*deltaT/particle->mass;
+	double beta = particle->charge*deltaT/(2*gamma*speed_of_light_normalized*particle->mass);
 
 	matrix.matrix[0][0] = 1;
-	matrix.matrix[0][1] = - beta*B.z/(2*gamma*speed_of_light_normalized);
-	matrix.matrix[0][2] = beta*B.y/(2*gamma*speed_of_light_normalized);
+	matrix.matrix[0][1] = - beta*B.z;
+	matrix.matrix[0][2] = beta*B.y;
 
-	matrix.matrix[1][0] = beta*B.z/(2*gamma*speed_of_light_normalized);
+	matrix.matrix[1][0] = beta*B.z;
 	matrix.matrix[1][1] = 1;
-	matrix.matrix[1][2] = - beta*B.x/(2*gamma*speed_of_light_normalized);
+	matrix.matrix[1][2] = - beta*B.x;
 
-	matrix.matrix[2][0] = - beta*B.y/(2*gamma*speed_of_light_normalized);
-	matrix.matrix[2][1] = beta*B.x/(2*gamma*speed_of_light_normalized);
+	matrix.matrix[2][0] = - beta*B.y;
+	matrix.matrix[2][1] = beta*B.x;
 	matrix.matrix[2][2] = 1;
 
 	Vector3d rightPart = particle->momentum + (E + (particle->momentum.vectorMult(B)/(2*particle->mass*gamma*speed_of_light_normalized)))*deltaT*particle->charge;
